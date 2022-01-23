70140ba3c85d951c4b88498d1cefee3f65a7c041
mahesh <30831566+mes-da@users.noreply.github.com>
2022-01-23 13:34:35 +0545

    2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Homescreen from "./screens/Homescreen";
 import { BrowserRouter, Route } from "react-router-dom";
+import Productdescscreen from "./screens/Productdescscreen";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Navbar />
 
       <BrowserRouter>
-        <Route exact path="/" component={Homescreen} />
+        <Route path="/" component={Homescreen} exact />
+        <Route exact path="/product/:id" component={Productdescscreen} />
       </BrowserRouter>
     </div>
   );
